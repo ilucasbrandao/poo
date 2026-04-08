7e3390522e0ecfdadea6c287d3a7ba91fd9c52a0
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-04-08 16:53:56 -0300

    Initial commit

---
+++ src/unidade01/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+package unidade01;
+
+public class PrimeiraClasse {
+    static void main(String[] args) {
+        System.out.println("Minha primeira classe de acordo com o conteúdo abordado no livro de Programação Orientada a Objeto.");
+    }
+}
